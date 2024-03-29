9135eb5646c6024f280a859efd481f0b06a08f2e
Simon Schnake <simon.schnake@desy.de>
2024-03-29 14:45:53 +0100

    delete old stuff

--- src/calohitflow/__init__.py
+++
@@ -1,8 +0,0 @@
-"""CaloHitFlow - Generating calorimeter showers hit by hit"""
-
-from calohitflow.model import CaloHitFlow
-
-__all__ = ["CaloHitFlow"]
-
-# Version of the CaloHitFlow package
-__version__ = "0.0.0"
--- src/calohitflow/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-"""modules"""
-
-from calohitflow.modules.encoder import Encoder
-from calohitflow.modules.flow import Flow
-from calohitflow.modules.point_flow import PointFlow
-
-__all__ = ["Flow", "Encoder", "PointFlow"]
